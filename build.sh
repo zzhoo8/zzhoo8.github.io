dd6a8b3c1981a823a3c9c65b0f7ad27f7e8141e1
zzhoo8 <zhaozhenhua@itianpin.com>
2016-01-30 02:03:45 +0800

    增加个人简历。。。

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #/bin/sh
 
-rm -rf _book
-
 gitbook build ./ --output=/Users/zzhoo8/Project/zzhoo8.github.io/
 
-mv _book/* /Users/zzhoo8/Project/zzhoo8.github.io/
+cp -rf _book/* /Users/zzhoo8/Project/zzhoo8.github.io/
 
+rm -r _book
